c5945e08aeab9bcb9d05ddc025e8087efe8a1c07
Chatheus <cl201605@g.unicamp.br>
2023-02-15 16:58:40 -0300

    Aula 15/02

---
+++ ex004.php
@@ -0,0 +1,17 @@
+<?php
+    $op = 3;
+
+    switch($op){
+        case 1:
+            cadastrar();
+            echo "Cadastro";
+            break;
+        case 2:
+            //emitirRelatorioVenda();
+            echo "Relatorios";
+            break;
+        default:
+            echo "Opcao invalida!";
+            break;
+    }
+?>
---
+++ ex005.php
@@ -0,0 +1,11 @@
+<?php 
+    $x = 2;
+
+    if($x%2 == 0) {
+        $resultado = "par";
+    } else {
+        $resuiltado = "ímpar";
+    }
+
+    $resultado = ($x%2)==0 ? "par" : "impar";
+?>
